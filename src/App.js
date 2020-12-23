f856e3e7f0b534557d27affe6d37da991bae8163
Kaeneth Dayao <kendayao@gmail.com>
2020-12-22 21:46:29 -0800

    finishedheader component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>Facebook clone</h1>
+      <Header/>
     </div>
   );
 }
