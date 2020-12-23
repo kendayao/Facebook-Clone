fea92341cac6e650cc2cc5a2808a24aa03872f88
Kaeneth Dayao <kendayao@gmail.com>
2020-12-22 20:41:57 -0800

    setup firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqVrBu6ynxI4APE9uZZ3HqZFNPHJAS_lY",
+    authDomain: "facebook-clone-d025c.firebaseapp.com",
+    projectId: "facebook-clone-d025c",
+    storageBucket: "facebook-clone-d025c.appspot.com",
+    messagingSenderId: "889223992208",
+    appId: "1:889223992208:web:9145539bcbf1b99f2a1915",
+    measurementId: "G-5TVEG1Q2LV"
+  };
