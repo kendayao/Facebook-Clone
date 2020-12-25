843c3e999e5ce47984cb23820cb59c313340c476
Kaeneth Dayao <kendayao@gmail.com>
2020-12-24 17:39:26 -0800

    sytled sidebar component

--- src/components/sidebar-row/SidebarRow.js
+++ src/components/sidebar-row/SidebarRow.js
@@ -4,7 +4,7 @@ import {Avatar} from '@material-ui/core'
 
 function SidebarRow({src, alt, Icon, title}) {
     return (
-        <div className='slidebarRow'>
+        <div className='sidebarRow'>
             {alt && <Avatar alt={alt}/>}
             {Icon && <Icon/>}
             <h4>{title}</h4>
