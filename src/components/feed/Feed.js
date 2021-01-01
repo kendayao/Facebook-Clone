00418af72ebad714612cc727376115b409a9f80c
Kaeneth Dayao <kendayao@gmail.com>
2020-12-31 16:30:47 -0800

    completed styling input section and input bottom section

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Feed.css'
 import StoryReel from '../storyreel/StoryReel'
+import MessageSender from '../message-sender/MessageSender'
 
 function Feed() {
     return (
         <div className='feed'>
             <StoryReel/>
+            <MessageSender />
         </div>
     )
 }
