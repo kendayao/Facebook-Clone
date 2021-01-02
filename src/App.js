55f7f3a9894f2e1942eb3575d669e46812163743
Kaeneth Dayao <kendayao@gmail.com>
2021-01-02 11:39:30 -0800

    styled login page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Header from './components/header/Header';
 import Feed from './components/feed/Feed';
-import Widgets from './components/widgets/Widgets'
+import Widgets from './components/widgets/Widgets';
+import Login from './components/login/Login';
 
 function App() {
+  const user=null;
   return (
     <div className="app">
-      <Header/>
+      {!user?(
+        <Login />
+      ):(
+        <>
+        <Header/>
 
       <div className='app__body'>
         <Sidebar/>
         <Feed />
         <Widgets/>
       </div>
+
+      </>
+      )}
+      
     </div>
   );
 }
