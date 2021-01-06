1da717b22936a18272279696a83f11a0040fc0e3
Kaeneth Dayao <kendayao@gmail.com>
2021-01-05 16:37:45 -0800

    finished media queries

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -100,7 +100,6 @@ function Login() {
                 </div>
                 </div>
             </Modal>
-
             <div className='login__logo'>
                 <img 
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt='facebook logo'
